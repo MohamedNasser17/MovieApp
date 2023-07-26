63d01ed116f16989d04b980fc41e6f421f7d509a
Mohamed Nasser <mohamed.nasser@agoda.com>
2023-07-26 08:55:39 +0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Mohamed Abdelhameed on 26/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
