382782f7e0c22d0ea4857c5316107acf2896af98
Mohamed Nasser <mohamed.nasser@agoda.com>
2023-07-26 09:22:49 +0700

    [Beta] Setup MoviesDiscovery Module
    - add MoviesDiscoveryView
    - add MoviesDiscoveryViewModel

---
+++ MovieApp/Modules/MoviesDiscovery/MoviesDiscoveryView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesDiscoveryView.swift
+//  MovieApp
+//
+//  Created by Mohamed Abdelhameed on 26/7/23.
+//
+
+import SwiftUI
+
+struct MoviesDiscoveryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoviesDiscoveryView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviesDiscoveryView()
+    }
+}
---
+++ MovieApp/Modules/MoviesDiscovery/MoviesDiscoveryViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MoviesDiscoveryViewModel.swift
+//  MovieApp
+//
+//  Created by Mohamed Abdelhameed on 26/7/23.
+//
+
+import Foundation
+
+protocol MoviesDiscoveryViewModelProtocol {
+}
+
+class MoviesDiscoveryViewModel: MoviesDiscoveryViewModelProtocol {
+}
